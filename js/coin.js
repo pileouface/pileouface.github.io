6db5e7184e3e00a8b32f6215bde96cbe2dd4e158
Adonis <adopi.naj@gmail.com>
2014-05-10 22:41:58 +0200

    fix many clics on the coin

--- js/coin.js
+++ js/coin.js
@@ -1,16 +1,34 @@
-//event for the click
-$('#flip').on('click',function(e){
+/*
+  main script
+ */
+
+//enable the click on the coin
+enableClick();
+
+/*
+  functions 
+*/
+
+//enable click on the coin
+function enableClick() {
+  //event for the click
+  $('#flip').bind('click',doFlip);
+}
+
+//do a coin flipping 
+function doFlip() {
   $('.front').addClass('flip');
+  disableClick();
   setTimeout(function() {
       resetCoin();
       setPileOuFace();
   },100);
-});
+}
 
-//event at the end of the animation
-$('#coin').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-    $('.flip').removeClass('flip');
-});
+//disable click during the animation
+function disableClick() {
+  $('#flip').unbind('click',doFlip);
+}
 
 //remove face and pile css classes
 function resetCoin() {
@@ -26,8 +44,14 @@ function setPileOuFace() {
     $('#coin .front').addClass('face');
 }
 
-//choose the random number
+//choose a random number
 function pileOuFace() {
   var alea = Math.random()*2;
   return (parseInt(alea) == 1 );
 }
+
+//event at the end of the animation
+$('#coin').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+    $('.flip').removeClass('flip');
+    enableClick();
+});
