f6c29214902a0655ebe1d4af3c8db3d81037f85f
Adonis Najimi <adopi.naj@gmail.com>
2014-05-11 14:40:24 +0200

    add random title

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 (function() {
   var animations = ['rollIn','bounceInDown'];
   var alternatives = ['pile','face']; 
+  var titles = ['Pile ou Face ?', 'Face ou Pile ?'];
   //choose the animation when we are loading the page
   var rand = Math.random()*2;
   $('#coin').addClass(animations[parseInt(rand)]);
   //choose the first value of the coin
   rand = Math.random()*2;
   $('.front').addClass(alternatives[parseInt(rand)]);
+  //choose the title with a random value
+   rand = Math.random()*2;
+  $('h1').text(titles[rand]);
 })();
